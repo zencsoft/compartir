e0fd545d33f9ce1297a1df816bc9b3fb435d568a
Andrés Zamora <zenyat123@gmail.com>
2019-11-08 15:56:28 -0500

    Compartir

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+	require_once("controladores/controlador_principal.php");
+
+	$principal = ControladorPrincipal::Principal();
---
+++ vistas/principal.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+
+	<meta charset = "utf-8">
+	<title>Compartir</title>
+
+	<link rel = "stylesheet" href = "https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+	<div class = "container-fluid">
+		
+		<div class = "container">
+			
+			<h1 class = "text-primary text-center">Dios te bendecirá por compartir</h1>
+
+		</div>
+
+	</div>
+	
+</body>
+
+</html>
